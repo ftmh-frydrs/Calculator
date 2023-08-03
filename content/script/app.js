bb1605f68c64c3ec60a3e6b901b3281b59f8d16f
Fatemeh <fatemehfaryadras7@gmail.com>
2023-08-03 23:10:20 +0330

    seconde commit

--- content/script/app.js
+++ content/script/app.js
@@ -2,7 +2,8 @@ const Calculate = document.querySelector('.calculate');
 const Curent = document.querySelector('.curent');
 let currentExpression = '';
 let result = '';
-let lastButtonType = ''; 
+let lastButtonType = '';
+let hasNumber = false; 
 
 function evaluateExpression(expression) {
     try {
@@ -16,31 +17,38 @@ function evaluateExpression(expression) {
 Calculate.addEventListener('click', (e) => {
     let buttonValue = e.target.textContent;
     buttonValue = buttonValue.replaceAll("x", "*");
-    console.log(buttonValue);
 
+    
     if (!isNaN(parseInt(buttonValue)) || buttonValue === '.') {
+     
         if ((buttonValue === '-' && (lastButtonType === '' || lastButtonType === 'operator')) || lastButtonType === 'equals') {
             currentExpression += buttonValue;
             Curent.value = currentExpression;
+            hasNumber = true; 
         } else {
             currentExpression += buttonValue;
             Curent.value = currentExpression;
+            hasNumber = true; 
         }
         lastButtonType = 'digit'; 
     } 
+    
     
     else if (buttonValue === 'AC') {
         currentExpression = '';
         Curent.value = '';
         lastButtonType = 'clear'; 
+        hasNumber = false; 
     }
 
+    
     else if (buttonValue === 'CE') {
         currentExpression = currentExpression.slice(0, -1);
         Curent.value = currentExpression;
         lastButtonType = 'backspace'; 
     }
-
+    
+    
     else if (buttonValue === '%') {
         result = evaluateExpression(currentExpression) / 100;
         Curent.value = result;
@@ -48,17 +56,23 @@ Calculate.addEventListener('click', (e) => {
         lastButtonType = 'operator'; 
     }
     
-    
-else if (buttonValue === '+' || buttonValue === '-' || buttonValue === '*' || buttonValue === '/') {
-    
-    if (lastButtonType !== 'operator' && lastButtonType !== 'clear') {
+
+    else if (buttonValue === '+' || buttonValue === '*' || buttonValue === '/') {
+        
+        if (hasNumber) {
+            currentExpression += buttonValue;
+            Curent.value = currentExpression;
+            lastButtonType = 'operator'; 
+        }
+    }
+
+    else if (buttonValue === '-' || !hasNumber) {
         currentExpression += buttonValue;
         Curent.value = currentExpression;
         lastButtonType = 'operator'; 
     }
-}
 
-   
+    
     else if (buttonValue === '=') {
         result = evaluateExpression(currentExpression);
         Curent.value = result;
