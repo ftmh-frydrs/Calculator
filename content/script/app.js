92535ccfa4376515648ea35e50ae35ed90c28d35
Fatemeh <fatemehfaryadras7@gmail.com>
2023-08-06 14:11:15 +0330

    finish

--- content/script/app.js
+++ content/script/app.js
@@ -3,7 +3,11 @@ const Curent = document.querySelector('.curent');
 let currentExpression = '';
 let result = '';
 let lastButtonType = '';
-let hasNumber = false; 
+let hasNumber = false;
+let lastButtonIsOperator = false; 
+
+
+const operators = ['+', '-', '*', '/' ]; 
 
 function evaluateExpression(expression) {
     try {
@@ -14,69 +18,67 @@ function evaluateExpression(expression) {
     }
 }
 
+
 Calculate.addEventListener('click', (e) => {
     let buttonValue = e.target.textContent;
     buttonValue = buttonValue.replaceAll("x", "*");
 
-    
     if (!isNaN(parseInt(buttonValue)) || buttonValue === '.') {
-     
-        if ((buttonValue === '-' && (lastButtonType === '' || lastButtonType === 'operator')) || lastButtonType === 'equals') {
+        if (
+            (buttonValue === '-' && (lastButtonType === '' || lastButtonType === 'operator')) ||
+            lastButtonType === 'equals'
+        ) {
             currentExpression += buttonValue;
             Curent.value = currentExpression;
-            hasNumber = true; 
+            hasNumber = true;
         } else {
-            currentExpression += buttonValue;
-            Curent.value = currentExpression;
-            hasNumber = true; 
+            if (!(buttonValue === '.' && currentExpression.endsWith('.'))) {
+                // Check if the button is a decimal point and there's already a decimal point at the end.
+                currentExpression += buttonValue;
+                Curent.value = currentExpression;
+                hasNumber = true;
+            }
         }
-        lastButtonType = 'digit'; 
-    } 
-    
-    
-    else if (buttonValue === 'AC') {
+        lastButtonType = 'digit';
+        lastButtonIsOperator = false;
+    } else if (buttonValue === 'AC') {
         currentExpression = '';
         Curent.value = '';
-        lastButtonType = 'clear'; 
-        hasNumber = false; 
-    }
-
-    
-    else if (buttonValue === 'CE') {
+        lastButtonType = 'clear';
+        hasNumber = false;
+        lastButtonIsOperator = false;
+    } else if (buttonValue === 'CE') {
         currentExpression = currentExpression.slice(0, -1);
         Curent.value = currentExpression;
-        lastButtonType = 'backspace'; 
-    }
-    
-    
-    else if (buttonValue === '%') {
+        lastButtonType = 'backspace';
+        lastButtonIsOperator = false;
+    } else if (buttonValue === '%') {
         result = evaluateExpression(currentExpression) / 100;
         Curent.value = result;
         currentExpression = result.toString();
-        lastButtonType = 'operator'; 
-    }
-    
-
-    else if (buttonValue === '+' || buttonValue === '*' || buttonValue === '/') {
-        
-        if (hasNumber) {
+        lastButtonType = 'operator';
+        lastButtonIsOperator = false;
+    } else if (buttonValue === '+' || buttonValue === '*' || buttonValue === '/') {
+        if (hasNumber && !lastButtonIsOperator) {
             currentExpression += buttonValue;
             Curent.value = currentExpression;
-            lastButtonType = 'operator'; 
+            lastButtonType = 'operator';
+            lastButtonIsOperator = true;
         }
-    }
-
-    else if (buttonValue === '-' || !hasNumber) {
-        currentExpression += buttonValue;
-        Curent.value = currentExpression;
-        lastButtonType = 'operator'; 
-    }
-
-    
-    else if (buttonValue === '=') {
+    } else if (buttonValue === '-' || !hasNumber) {
+        if (!lastButtonIsOperator) {
+            currentExpression += buttonValue;
+            Curent.value = currentExpression;
+            lastButtonType = 'operator';
+            lastButtonIsOperator = true;
+        }
+    } else if (buttonValue === '=') {
         result = evaluateExpression(currentExpression);
         Curent.value = result;
         currentExpression = result.toString();
-        lastButtonType = 'equals'; 
+        lastButtonType = 'equals';
+        lastButtonIsOperator = false;
     }
 });
+
+
